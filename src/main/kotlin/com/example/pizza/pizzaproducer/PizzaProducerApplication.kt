65fe3f67a97e71f719249d681415553de9d29a11
Tyson McNulty <tyson.mcnulty@gmail.com>
2018-08-08 08:39:01 -0400

    First commit - Spring/Kotlin app

---
+++ src/main/kotlin/com/example/pizza/pizzaproducer/PizzaProducerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pizza.pizzaproducer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PizzaProducerApplication
+
+fun main(args: Array<String>) {
+    runApplication<PizzaProducerApplication>(*args)
+}
